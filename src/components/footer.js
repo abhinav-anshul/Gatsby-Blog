ab8e93514f7c229768db6aa4e3a182c556870b83
abhinav anshul <abhinavanshul4u@gmail.com>
2019-08-03 18:59:32 +0530

    footer added

---
+++ src/components/footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+    return(
+        <footer>
+            <p>Created by Abhinav Anshul, © 2019</p>
+        </footer>
+    )
+}
+
+export default footer
