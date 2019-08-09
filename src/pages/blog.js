d9ed19a3d7ae622d0c72979c92e2e9722d0ba56d
abhinav anshul <abhinavanshul4u@gmail.com>
2019-08-10 00:12:38 +0530

    finished off a markdown based gatsby powered web application

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -2,6 +2,7 @@ import React from 'react'
 import {Link, graphql, useStaticQuery} from 'gatsby'
 
 import Layout from '../components/layout'
+import blogStyles from './blog.module.scss'
  
 
 
@@ -30,10 +31,10 @@ const BlogPage = () => {
         <div>
             <Layout>
                 <h1>Blog</h1>
-                <ol>
+                <ol className={blogStyles.posts}>
                     {data.allMarkdownRemark.edges.map((edge) => {
                         return(
-                            <li>
+                            <li className={blogStyles.post}>
                                 <Link to={`/blog/${edge.node.fields.slug}`}>
                                     <h2>{edge.node.frontmatter.title}</h2>
                                     <p>{edge.node.frontmatter.date}</p>
